9e030e031168c3202d7495f12392238d50ea884c
Joel Colucci <joelcolucci@gmail.com>
2016-10-12 06:29:38 -0400

    tests: add test scaffold with unittest

---
+++ tests/test_register.py
@@ -0,0 +1,18 @@
+"""Unit tests for register module
+"""
+
+
+import unittest
+
+
+class RegisterTestCase(unittest.TestCase):
+    """Test suite for register module"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
